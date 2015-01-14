f89551eebda94df072c95fc81cc3130ef3e46a38
Ian <typppo@gmail.com>
2015-01-13 20:55:02 -0800

    reformatting everything

--- src/com/judyian/minion/Barometer.java
+++ src/com/judyian/minion/Barometer.java
@@ -11,49 +11,52 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 public class Barometer implements SensorEventListener {
-    private SensorManager sensorManager;
-    private FileWriter fileWriter;
+	private SensorManager sensorManager;
+	private FileWriter fileWriter;
 
-    // Pressure in mBars
-    private float lastPressure = -1;
+	// Pressure in mBars
+	private float lastPressure = -1;
 
-    public Barometer(Context context, FileWriter fileWriter) {
-        sensorManager = (SensorManager) context.getSystemService(Service.SENSOR_SERVICE);
-        this.fileWriter = fileWriter;
-    }
+	public Barometer(Context context, FileWriter fileWriter) {
+		sensorManager = (SensorManager) context
+				.getSystemService(Service.SENSOR_SERVICE);
+		this.fileWriter = fileWriter;
+	}
 
-    public void startRecordingAltitude() {
-        Sensor pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
-        if (pressureSensor == null) {
-            System.out.println("No pressure sensor on this device.");
-        } else {
-            sensorManager.registerListener(this, pressureSensor, SensorManager.SENSOR_DELAY_UI);
-        }
-    }
+	public void startRecordingAltitude() {
+		Sensor pressureSensor = sensorManager
+				.getDefaultSensor(Sensor.TYPE_PRESSURE);
+		if (pressureSensor == null) {
+			System.out.println("No pressure sensor on this device.");
+		} else {
+			sensorManager.registerListener(this, pressureSensor,
+					SensorManager.SENSOR_DELAY_UI);
+		}
+	}
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // TODO Auto-generated method stub
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        lastPressure = event.values[0];
-        try {
-            Long timestampSeconds = System.currentTimeMillis() / 1000;
-            fileWriter
-                    .write(timestampSeconds.toString() + "," + getEstimatedAltitudeInFeet() + ";");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		lastPressure = event.values[0];
+		try {
+			Long timestampSeconds = System.currentTimeMillis() / 1000;
+			fileWriter.write(timestampSeconds.toString() + ","
+					+ getEstimatedAltitudeInFeet() + ";");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public double getEstimatedAltitudeInFeet() {
-        if (lastPressure == -1) {
-            return lastPressure;
-        }
-        // NOAA http://www.srh.noaa.gov/images/epz/wxcalc/pressureAltitude.pdf
-        return (1 - Math.pow(lastPressure / 1013.25, 0.190284)) * 145366.45;
-    }
+	public double getEstimatedAltitudeInFeet() {
+		if (lastPressure == -1) {
+			return -1;
+		}
+		// NOAA http://www.srh.noaa.gov/images/epz/wxcalc/pressureAltitude.pdf
+		return (1 - Math.pow(lastPressure / 1013.25, 0.190284)) * 145366.45;
+	}
 }
--- src/com/judyian/minion/MainActivity.java
+++ src/com/judyian/minion/MainActivity.java
@@ -20,141 +20,144 @@ import android.view.SurfaceView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    // Runs every 5 seconds.
-    private Handler timerHandler = new Handler();
-    private Runnable timerRunnable = new Runnable() {
-        @Override
-        public void run() {
-            takePicture();
-            timerHandler.postDelayed(this, 5000);
-        }
-    };
+	// Runs every 5 seconds.
+	private Handler timerHandler = new Handler();
+	private Runnable timerRunnable = new Runnable() {
+		@Override
+		public void run() {
+			takePicture();
+			timerHandler.postDelayed(this, 5000);
+		}
+	};
 
-    // Runs every 5 min.
-    private Handler timerHandler5min = new Handler();
-    private Runnable timerRunnable5min = new Runnable() {
-        @Override
-        public void run() {
-            uploadFile();
-            timerHandler.postDelayed(this, 1000 * 60 * 5);
-        }
-    };
+	// Runs every 5 min.
+	private Handler timerHandler5min = new Handler();
+	private Runnable timerRunnable5min = new Runnable() {
+		@Override
+		public void run() {
+			uploadFile();
+			timerHandler.postDelayed(this, 1000 * 60 * 5);
+		}
+	};
 
-    private Tracker tracker;
-    private Barometer barometer;
-    private FileWriter locationFileWriter;
-    private FileWriter altitudeFileWriter;
+	private Tracker tracker;
+	private Barometer barometer;
+	private FileWriter locationFileWriter;
+	private FileWriter altitudeFileWriter;
 
-    private Uploader uploader;
-    private String lastFileName;
+	private Uploader uploader;
+	private String lastFileName;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        uploader = new Uploader();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		uploader = new Uploader();
 
-        try {
-            locationFileWriter = new FileWriter(Environment.getExternalStorageDirectory() +
-                    "/Text/location.txt", true /* append */);
-            altitudeFileWriter = new FileWriter(Environment.getExternalStorageDirectory() +
-                    "/Text/altitude.txt", true /* append */);
-        } catch (IOException e) {
-            System.out.println("Cannot get location file");
-            e.printStackTrace();
-        }
-        tracker = new Tracker(getBaseContext(), locationFileWriter);
-        tracker.startLocationTracking();
+		try {
+			locationFileWriter = new FileWriter(
+					Environment.getExternalStorageDirectory()
+							+ "/Text/location.txt", true /* append */);
+			altitudeFileWriter = new FileWriter(
+					Environment.getExternalStorageDirectory()
+							+ "/Text/altitude.txt", true /* append */);
+		} catch (IOException e) {
+			System.out.println("Cannot get location file");
+			e.printStackTrace();
+		}
+		tracker = new Tracker(getBaseContext(), locationFileWriter);
+		tracker.startLocationTracking();
 
-        barometer = new Barometer(getBaseContext(), altitudeFileWriter);
-        barometer.startRecordingAltitude();
+		barometer = new Barometer(getBaseContext(), altitudeFileWriter);
+		barometer.startRecordingAltitude();
 
-        timerHandler.postDelayed(timerRunnable, 0);
-        timerHandler5min.postDelayed(timerRunnable5min, 5000);
+		timerHandler.postDelayed(timerRunnable, 0);
+		timerHandler5min.postDelayed(timerRunnable5min, 5000);
 
-        System.out.println("Initialized minion.");
-    }
+		System.out.println("Initialized minion.");
+	}
 
-    @Override
-    protected void onStop() {
-        try {
-            locationFileWriter.flush();
-            locationFileWriter.close();
-            altitudeFileWriter.flush();
-            altitudeFileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	protected void onStop() {
+		try {
+			locationFileWriter.flush();
+			locationFileWriter.close();
+			altitudeFileWriter.flush();
+			altitudeFileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
-    SurfaceView surface;
-    Camera camera;
+	SurfaceView surface;
+	Camera camera;
 
-    private void takePicture() {
-        Toast.makeText(getApplicationContext(), "Image snapshot Started", Toast.LENGTH_SHORT)
-                .show();
-        // here below "this" is activity context.
-        surface = new SurfaceView(this);
-        camera = Camera.open();
-        try {
-            camera.setPreviewDisplay(surface.getHolder());
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        camera.startPreview();
-        camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-    }
+	private void takePicture() {
+		Toast.makeText(getApplicationContext(), "Image snapshot Started",
+				Toast.LENGTH_SHORT).show();
+		// here below "this" is activity context.
+		surface = new SurfaceView(this);
+		camera = Camera.open();
+		try {
+			camera.setPreviewDisplay(surface.getHolder());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		camera.startPreview();
+		camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+	}
 
-    ShutterCallback shutterCallback = new ShutterCallback() {
-        public void onShutter() {
-        }
-    };
+	ShutterCallback shutterCallback = new ShutterCallback() {
+		public void onShutter() {
+		}
+	};
 
-    PictureCallback rawCallback = new PictureCallback() {
-        public void onPictureTaken(byte[] data, Camera camera) {
-        }
-    };
+	PictureCallback rawCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+		}
+	};
 
-    PictureCallback jpegCallback = new PictureCallback() {
-        public void onPictureTaken(byte[] data, Camera camera) {
-            FileOutputStream outStream = null;
-            try {
-                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-                String imageFileName = "JPEG_" + timeStamp + "_";
-                File storageDir = Environment
-                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+	PictureCallback jpegCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			FileOutputStream outStream = null;
+			try {
+				String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+						.format(new Date());
+				String imageFileName = "JPEG_" + timeStamp + "_";
+				File storageDir = Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
-                String fullPath = storageDir + "/" + imageFileName + ".jpg";
-                lastFileName = fullPath;
-                System.out.println("Saving pic to " + fullPath);
-                outStream = new FileOutputStream(fullPath);
-                outStream.write(data);
-                outStream.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                camera.stopPreview();
-                camera.release();
-                camera = null;
-                Toast.makeText(getApplicationContext(), "Image snapshot done", Toast.LENGTH_LONG)
-                        .show();
-                System.out.println("Snapshot saved");
+				String fullPath = storageDir + "/" + imageFileName + ".jpg";
+				lastFileName = fullPath;
+				System.out.println("Saving pic to " + fullPath);
+				outStream = new FileOutputStream(fullPath);
+				outStream.write(data);
+				outStream.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				camera.stopPreview();
+				camera.release();
+				camera = null;
+				Toast.makeText(getApplicationContext(), "Image snapshot done",
+						Toast.LENGTH_LONG).show();
+				System.out.println("Snapshot saved");
 
-            }
-        }
-    };
+			}
+		}
+	};
 
-    private void uploadFile() {
-        uploader.uploadFile(new File(lastFileName));
-    }
+	private void uploadFile() {
+		uploader.uploadFile(new File(lastFileName));
+	}
 }
